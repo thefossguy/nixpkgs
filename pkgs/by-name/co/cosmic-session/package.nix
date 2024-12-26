899fa3ab30dbb98371ee744aa7c9b7208d161547
Pratham Patel <prathampatel@thefossguy.com>
2024-12-26 14:27:26 +0530

    cosmic-session: use `libcosmicAppHook` instead of `makeBinaryWrapper`

--- pkgs/by-name/co/cosmic-session/package.nix
+++ pkgs/by-name/co/cosmic-session/package.nix
@@ -1,60 +1,81 @@
 {
-  lib,
-  fetchFromGitHub,
   bash,
-  rustPlatform,
-  just,
   dbus,
+  fetchFromGitHub,
+  just,
+  lib,
+  nix-update-script,
+  rustPlatform,
   stdenv,
   xdg-desktop-portal-cosmic,
 }:
-rustPlatform.buildRustPackage rec {
+
+rustPlatform.buildRustPackage {
   pname = "cosmic-session";
-  version = "1.0.0-alpha.2";
+  version = "epoch-1.0.0-alpha.2";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-session";
-    rev = "epoch-${version}";
-    hash = "sha256-rkzcu5lXKVQ5RfilcKQjTzeKZv+FpqrtARZgGGlYKK4=";
+    rev = "refs/tags/epoch-1.0.0-alpha.2";
+    hash = "sha256-M6OIh8llhyWHB9S5inU06ORvnhj3hPUoPCuUqLwNEBU=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-rK0tWckPvp31BT+K0pfs/mk/Z4XkwlOIwJEZwpYphJE=";
+  cargoHash = "sha256-iYObxjWJUKgZKGTkqtYgQK4758k0EYZGhIAM/oLxxso=";
 
   postPatch = ''
     substituteInPlace Justfile \
       --replace-fail '{{cargo-target-dir}}/release/cosmic-session' 'target/${stdenv.hostPlatform.rust.cargoShortTarget}/release/cosmic-session'
     substituteInPlace data/start-cosmic \
-      --replace-fail '/usr/bin/cosmic-session' "${placeholder "out"}/bin/cosmic-session" \
-      --replace-fail '/usr/bin/dbus-run-session' "${lib.getBin dbus}/bin/dbus-run-session"
+      --replace-fail /usr/bin/cosmic-session "''${!outputBin}/bin/cosmic-session" \
+      --replace-fail /usr/bin/dbus-run-session '${lib.getExe' dbus "dbus-run-session"}' \
+      --replace-fail 'systemctl --user import-environment XDG_SESSION_TYPE XDG_CURRENT_DESKTOP' '${lib.getExe' dbus "dbus-update-activation-environment"} --systemd PATH XDG_SESSION_CLASS XDG_CONFIG_DIRS XDG_DATA_DIRS XDG_SESSION_DESKTOP XDG_CURRENT_DESKTOP XDG_SESSION_TYPE DCONF_PROFILE XDG_DESKTOP_PORTAL_DIR DISPLAY WAYLAND_DISPLAY XMODIFIERS XCURSOR_SIZE XCURSOR_THEME GDK_PIXBUF_MODULE_FILE GIO_EXTRA_MODULES GTK_IM_MODULE QT_PLUGIN_PATH QT_QPA_PLATFORMTHEME QT_STYLE_OVERRIDE QT_IM_MODULE NIXOS_OZONE_WL &>/dev/null'
     substituteInPlace data/cosmic.desktop \
-      --replace-fail '/usr/bin/start-cosmic' "${placeholder "out"}/bin/start-cosmic"
+      --replace-fail /usr/bin/start-cosmic "''${!outputBin}/bin/start-cosmic"
   '';
 
   buildInputs = [ bash ];
   nativeBuildInputs = [ just ];
 
   dontUseJustBuild = true;
+  dontUseJustCheck = true;
 
   justFlags = [
     "--set"
     "prefix"
     (placeholder "out")
+    "--set"
+    "cosmic_dconf_profile"
+    "cosmic"
   ];
 
   env.XDP_COSMIC = lib.getExe xdg-desktop-portal-cosmic;
 
-  passthru.providedSessions = [ "cosmic" ];
+  postInstall = ''
+    mkdir -p $out/etc
+    cp -r data/dconf $out/etc/
+  '';
+
+  passthru = {
+    updateScript = nix-update-script {
+      extraArgs = [
+        "--version-regex"
+        "epoch-(.*)"
+      ];
+    };
+    providedSessions = [ "cosmic" ];
+  };
 
   meta = with lib; {
     homepage = "https://github.com/pop-os/cosmic-session";
-    description = "Session manager for the COSMIC desktop environment";
+    description = "Session manager for the COSMIC Desktop Environment";
     license = licenses.gpl3Only;
     mainProgram = "cosmic-session";
     maintainers = with maintainers; [
       a-kenji
       nyabinary
+      thefossguy
     ];
     platforms = platforms.linux;
   };
